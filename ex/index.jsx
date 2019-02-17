ca179798b4cb3f9eb276bbf3c8b3072aeebd95fd
Marcelo Gumiero <mgumiero9@gmail.com>
2019-02-17 14:41:16 -0300

    component with param

--- ex/index.jsx
+++ ex/index.jsx
@@ -2,4 +2,4 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Component from './component.jsx';
 
-ReactDOM.render(<Component/>, document.getElementById('app'));
+ReactDOM.render(<Component value={'show'}/>, document.getElementById('app'));
